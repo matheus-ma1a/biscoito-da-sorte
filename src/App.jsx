80dd93f0aeb9bd2a8e9dfecebddff754d5cca11f
matheus maia <matheussantosmaia@gmail.com>
2023-01-20 11:54:25 -0300

    adiciona o fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function getMessage(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  }
+
+  getMessage()
+
   return (
     <div className="App">
       <div className="main">
         <p className="textmain">sorte do dia</p>
-        <button></button>
+        <button><img src="/icons8-dice-50.png" alt="" /></button>
       </div>
     </div>
   )
