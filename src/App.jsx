87e5788c46ba20147a33d2e6fce78857ebcdf88d
matheus <matheussantosmaia@gmail.com>
2023-01-20 14:18:11 -0300

    cria a funcionalidade do botao que gera as mensagens para o usuario

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setmessage] = useState()
 
   function getMessage(){
+     
     fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then((response) => response.json())
-    .then((json) => console.log(json));
-  }
+    .then((json) => setmessage(json.title));
 
-  getMessage()
+    
+  }
 
   return (
     <div className="App">
       <div className="main">
-        <p className="textmain">sorte do dia</p>
-        <button><img src="/icons8-dice-50.png" alt="" /></button>
+        <p className="textmain">{message}</p>
+        <button onClick={getMessage} ><img src="/icons8-dice-50.png" alt="" /></button>
       </div>
     </div>
   )
 }
 
 export default App
+ 
