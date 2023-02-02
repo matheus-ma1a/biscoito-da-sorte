f2700f774e53437d39e27927460396b4dd9d4070
matheus <matheussantosmaia@gmail.com>
2023-02-02 00:14:49 -0300

    adiciona mais algumas animacoes no botao

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   const [message, setmessage] = useState()
   const [load, setLoad] = useState()
+  const [rotate, setRotate] = useState()
 
   function getMessage(){
     setLoad(true)
@@ -26,18 +27,28 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }} 
+      className="App">
+
       <div className="main">
         <p className="textmain">{ load ? 'loading...' : message}</p>
         <motion.button 
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 1.1 }}
-          onClick={getMessage}
-          ><img src="/icons8-dice-50.png" alt="" /></motion.button>
+          onClick={getMessage}>
+
+          <motion.img 
+          onClick={()=>{setRotate(!rotate)}} 
+          src="/icons8-dice-50.png" alt=""
+          animate={{rotate: rotate? 360 : 0}} />
+
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
 export default App
- 
